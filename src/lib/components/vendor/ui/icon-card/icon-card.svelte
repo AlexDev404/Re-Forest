9d102538d9d8a0a7986fa6cda8d3db36a5ceddee
AlexDev404 <alexdev404@users.noreply.github.com>
2024-11-12 23:52:45 -0600

    Add IconCard component and update page layout

---
+++ src/lib/components/vendor/ui/icon-card/icon-card.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import * as Avatar from '$lib/components/vendor/ui/avatar';
+	let src: string;
+	export { src as avatarSrc };
+	export let avatarFallback: string;
+	export let title: string;
+	export let description: string;
+</script>
+
+<div class="w-full">
+	<div class="flex justify-between space-x-4">
+		<Avatar.Root>
+			<Avatar.Image {src} />
+			<Avatar.Fallback>{avatarFallback}</Avatar.Fallback>
+		</Avatar.Root>
+		<div class="space-y-1">
+			<h4 class="text-sm font-semibold">{title}</h4>
+			<p class="text-sm">{description}</p>
+			<div class="flex items-center pt-2">
+				<slot name="content" />
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/lib/components/vendor/ui/icon-card/index.ts
@@ -0,0 +1,12 @@
+import { LinkPreview as HoverCardPrimitive } from "bits-ui";
+
+import Content from "./icon-card.svelte";
+const Root = HoverCardPrimitive.Root;
+const Trigger = HoverCardPrimitive.Trigger;
+
+export {
+	Content, Root as HoverCard,
+	Content as HoverCardContent,
+	Trigger as HoverCardTrigger, Root, Trigger
+};
+
