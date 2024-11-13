6e0e3e4f04c0365139541ac48961023987601d33
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-11-12 23:52:45 -0600

    Add IconCard component and update page layout

--- src/routes/view/+page.svelte
+++ src/routes/view/+page.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+	import IconCard from '$lib/components/vendor/ui/icon-card/icon-card.svelte';
+	import { Calendar } from 'lucide-svelte';
+	import { onMount } from 'svelte';
 
-    onMount(() => {
-       
-    });
-
+	onMount(() => {});
 </script>
 
 <page>
-    <main class="flex mx-4 flex-col items-start gap-12">
-        <article class="flex items-start self-stretch">
-            <h1 class="text-4xl font-semibold">Today</h1>
-        </article>
+	<main class="mx-6 my-10 flex flex-col items-start gap-12">
+		<article class="flex items-start self-stretch">
+			<h1 class="text-3xl font-semibold">Today</h1>
+		</article>
 
-    </main>
+		<IconCard
+			avatarSrc="https://github.com/sveltejs.png"
+			avatarFallback="SK"
+			title="@sveltejs"
+			description="Cybernetically enhanced web apps"
+		>
+			<div slot="content">
+				<Calendar class="mr-2 h-4 w-4 opacity-70" />
+				<span class="text-xs text-muted-foreground"> Joined September 2022 </span>
+			</div>
+		</IconCard>
+	</main>
 </page>
