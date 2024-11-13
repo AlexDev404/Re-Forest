3ed8c3e8718f60d3ce9a94a2bef5e8999352e4b1
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-11-12 23:34:29 -0600

    Initialize Code. Add Navigation Bar. Add first page.

---
+++ src/routes/view/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+       
+    });
+
+</script>
+
+<page>
+    <main class="flex mx-4 flex-col items-start gap-12">
+        <article class="flex items-start self-stretch">
+            <h1 class="text-4xl font-semibold">Today</h1>
+        </article>
+
+    </main>
+</page>
