b704c369e2813b84adcc40d5eecf70a9370e24d9
AlexDev404 <alexdev404@users.noreply.github.com>
2024-11-13 01:08:25 -0600

    Fix the navigation primitive

--- src/lib/components/vendor/ui/navigator/navigator.svelte
+++ src/lib/components/vendor/ui/navigator/navigator.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import { cn } from "$lib/components/vendor/shadcn/utils";
-	import { Menubar as MenubarPrimitive } from "bits-ui";
+	import { cn } from '$lib/components/vendor/shadcn/utils';
+	import { Menubar as MenubarPrimitive } from 'bits-ui';
 
 	type $$Props = MenubarPrimitive.Props;
 
-	let className: $$Props["class"] = undefined;
+	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
 
 <MenubarPrimitive.Root
 	class={cn(
-		"bg-primary text-background flex h-fit items-center justify-center space-x-1 rounded-md border p-1 shadow-sm",
+		'flex h-fit items-center justify-between lg:justify-center lg:space-x-1 self-stretch rounded-md border bg-primary p-1 text-background shadow-sm',
 		className
 	)}
 >
