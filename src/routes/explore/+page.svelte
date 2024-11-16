05c7cdda0a5c3121b9dd1aed014d621f4925f184
AlexDev404 <alexdev404@users.noreply.github.com>
2024-11-16 01:41:36 -0600

    Tuck away some details in dialog boxes

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import * as Alert from '$lib/components/vendor/ui/alert';
+	import * as Dialog from '$lib/components/vendor/ui/dialog';
 	import TreeData from '$lib/data/trees.json';
 	import { type Tree } from '$lib/types/Tree';
 	import { BadgePlus } from 'lucide-svelte';
@@ -36,73 +37,112 @@
 							alt={tree.name}
 						/>
 						<div class="absolute left-0 right-0 top-0 bg-black bg-opacity-50 p-4 text-white">
-							<h1 class="mb-2 text-3xl font-bold">{tree.name}</h1>
+							<h1 class="mb-1 text-3xl font-bold">{tree.name}</h1>
 							<p>{tree.description}</p>
-
-							<div class="block justify-between">
-								<div class="text-md">
-									<p>Height: {tree.height}</p>
-									<p>Age: {tree.age}</p>
-								</div>
-								<div>
-									<!-- <p>Location: {tree.lat} {tree.lng}</p> -->
-									<div
-										class="location-data mt-2 text-sm font-light"
-										use:inview
-										on:inview_enter={(isVisible) => {
-											if (isVisible && !tree.location_readable) {
-												// Do something when the div is visible
-												fetch(
-													`https://geocode.maps.co/reverse?format=jsonv2&lat=${tree.lat}&lon=${tree.lng}&api_key=67383f9b5a5d8533348772gbf5de3c7`,
-													{ signal: controller.signal }
-												)
-													.then((r) => r.json())
-													.then((d) => {
-														data = d;
-														tree.location_readable = data.display_name;
-													})
-													.catch((err) => {
-														if (err.name === 'AbortError') {
-															console.log('Fetch aborted');
-														} else {
-															console.error('Fetch error:', err);
-														}
-													});
-												// console.log('This div is currently in view');
-											}
-										}}
-										on:inview_leave={(event) => {
-											console.log(index);
-											// Check one more time to see if the element is still in view
-											if (
-												document.getElementById(`tree__${index}`)?.getBoundingClientRect().top <
-												window.innerHeight
-											) {
-												// console.log('Still in view');
-												return;
-											}
-											if (
-												(event.scrollDirection =
-													('down' && index === 1) || index === 0 || !signal_ready || !index)
-											) {
-												return;
-											}
-											// Do something when the div is not visible
-											controller.abort();
-											// Create a new controller
-											controller = new AbortController(); // Weird lol
-											console.log(event, 'This div is not in view');
-										}}
-										bind:this={locationElement}
-									>
-										{#if tree}
-											{tree.location_readable}
-										{:else}
-											Location unavailable.
-										{/if}
-									</div>
+							<div>
+								<!-- <p>Location: {tree.lat} {tree.lng}</p> -->
+								<div
+									class="location-data mb-2 text-sm font-light"
+									use:inview
+									on:inview_enter={(isVisible) => {
+										if (isVisible && !tree.location_readable) {
+											// Do something when the div is visible
+											fetch(
+												`https://geocode.maps.co/reverse?format=jsonv2&lat=${tree.lat}&lon=${tree.lng}&api_key=67383f9b5a5d8533348772gbf5de3c7`,
+												{ signal: controller.signal }
+											)
+												.then((r) => r.json())
+												.then((d) => {
+													data = d;
+													tree.location_readable = data.display_name;
+												})
+												.catch((err) => {
+													if (err.name === 'AbortError') {
+														console.log('Fetch aborted');
+													} else {
+														console.error('Fetch error:', err);
+													}
+												});
+											// console.log('This div is currently in view');
+										}
+									}}
+									on:inview_leave={(event) => {
+										console.log(index);
+										// Check one more time to see if the element is still in view
+										if (
+											document.getElementById(`tree__${index}`)?.getBoundingClientRect().top <
+											window.innerHeight
+										) {
+											// console.log('Still in view');
+											return;
+										}
+										if (
+											(event.scrollDirection =
+												('down' && index === 1) || index === 0 || !signal_ready || !index)
+										) {
+											return;
+										}
+										// Do something when the div is not visible
+										controller.abort();
+										// Create a new controller
+										controller = new AbortController(); // Weird lol
+										console.log(event, 'This div is not in view');
+									}}
+									bind:this={locationElement}
+								>
+									{#if tree}
+										{tree.location_readable}
+									{:else}
+										Location unavailable.
+									{/if}
 								</div>
 							</div>
+							<div
+								class="tree_health my-4 flex items-center justify-start space-x-3 rounded-md border border-background px-2 py-2"
+							>
+								<!-- Display the health as dots -->
+								<div class="dots flex items-center justify-start space-x-2">
+									{#each Array(Math.round(parseInt(tree.health.replace(/[^0-9]/g, '')) / 20)) as _, i}
+										<span
+											class="dot h-4 w-4 rounded-full"
+											style="background-color: {`hsl(${i * 30}, 70%, 50%)`}"
+										/>
+									{/each}
+								</div>
+								<p class="font-semibold">
+									{#snippet healthStatus()}
+										{@const healthScore =
+											Math.round(parseInt(tree.health.replace(/[^0-9]/g, '')) / 20) * 30}
+										<!-- {healthScore} -->
+										{#if healthScore < 90}
+											Poor
+										{:else if healthScore <= 90}
+											Fair
+										{:else if healthScore <= 120}
+											Good
+										{:else if healthScore > 120}
+											Excellent
+										{:else}
+											{healthScore}
+										{/if}
+									{/snippet}
+
+									{@render healthStatus()}
+								</p>
+							</div>
+							<Dialog.Root>
+								<Dialog.Trigger class="underline font-light">More details</Dialog.Trigger>
+
+								<Dialog.Content class="sm:max-w-[425px]" id="add-tree">
+									<Dialog.Header>
+										<Dialog.Title>Tree details</Dialog.Title>
+									</Dialog.Header>
+									<div class="text-md">
+										<p>Height: {tree.height}</p>
+										<p>Age: {tree.age}</p>
+									</div>
+								</Dialog.Content>
+							</Dialog.Root>
 						</div>
 						<div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-25 p-4 pb-20">
 							<Alert.Root>
