5e9d92408c55a223eea690fdac4468c6f30ee7b5
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-11-17 00:02:36 -0600

    Add initial site location page component

---
+++ src/lib/components/vendor/ui/menuitem/menuitem.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div
+	role="menuitem"
+	tabindex="-1"
+	aria-expanded="false"
+	class="flex w-full cursor-pointer items-center rounded-sm border border-slate-100 px-2 py-1.5 text-sm outline-none hover:bg-accent hover:text-foreground data-[highlighted]:bg-accent data-[state=open]:bg-accent"
+>
+	<div class="flex items-center space-x-2">
+		<icon class="ml-auto">
+			<slot name="start-icon"/>
+		</icon>
+
+		<span>{title}</span>
+	</div>
+	<icon class="ml-auto">
+		<slot />
+	</icon>
+</div>
---
+++ src/routes/configure/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import MenuItem from '$lib/components/vendor/ui/menuitem/menuitem.svelte';
+	import { ChevronRight, Thermometer } from 'lucide-svelte';
+	import { onMount } from 'svelte';
+	let data: any;
+
+	onMount(() => {
+		console.log('Settings page loaded');
+	});
+</script>
+
+<svelte:head>
+	<title>Re:Forest :: View</title>
+</svelte:head>
+<page class="overflow-y-auto">
+	<main class="mx-6 my-10 flex flex-col items-start gap-9">
+		<article class="flex flex-col items-start self-stretch">
+			<h1 class="text-2xl font-semibold">Settings</h1>
+		</article>
+		<optionsList class="flex w-full flex-col items-start gap-2">
+			<MenuItem title="Profile">
+				<svelte:fragment slot="start-icon">
+					<Thermometer class="h-4 w-4" />
+				</svelte:fragment>
+				<ChevronRight class="h-4 w-4" />
+			</MenuItem>
+			<MenuItem title="Profile">
+				<svelte:fragment slot="start-icon">
+					<Thermometer class="h-4 w-4" />
+				</svelte:fragment>
+				<ChevronRight class="h-4 w-4" />
+			</MenuItem>
+		</optionsList>
+		<article class="w-full pb-12 text-center text-sm font-light text-slate-600">
+			It seems that you have reached the end.
+		</article>
+	</main>
+</page>
+
+<style>
+	:global(body) {
+		overflow: auto;
+	}
+</style>
