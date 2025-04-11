37d7ffdc3a7d6106e98f94f33fc0538c35036f02
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-04-11 13:08:34 -0600

    Add example environment file and initial database migration script; create login page layout

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    
+</script>
+
+<svelte:head>
+	<title>Re:Forest :: Login to the site</title>
+</svelte:head>
+<page class="overflow-y-auto">
+	<main class="mx-6 my-10 flex min-h-[80vh] flex-col items-start gap-9">
+    </main>
+</page>
