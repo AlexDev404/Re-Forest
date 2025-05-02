da26b7fcb497cdcbcd10e4098fc2f100edb9a70a
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-05-02 05:45:52 +0000

    fmt: src/routes/manage

--- src/routes/manage/+page.svelte
+++ src/routes/manage/+page.svelte
@@ -1,27 +1,20 @@
 <script lang="ts">
-	import { toast } from '@zerodevx/svelte-toast';
-	import { goto } from '$app/navigation';
 	import { Button } from '$lib/components/vendor/ui/button';
 	import IconCard from '$lib/components/vendor/ui/icon-card/icon-card.svelte';
-	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
-	
 
 	export let data: PageData;
 
-	console.log(data); 
+	console.log(data);
 
 	const trees = data.trees;
 
 	function confirmDelete(event: SubmitEvent) {
-	const form = event.currentTarget as HTMLFormElement;
-	const confirmed = confirm("Are you sure you want to delete this tree?");
-	if (confirmed) form.submit();
+		const form = event.currentTarget as HTMLFormElement;
+		const confirmed = confirm('Are you sure you want to delete this tree?');
+		if (confirmed) form.submit();
 	}
-
-
 </script>
-
 
 <svelte:head>
 	<title>Re:Forest :: Manage Trees</title>
@@ -36,25 +29,24 @@
 			</div>
 		</article>
 		{#if trees.length > 0}
-		{#each trees as tree}
-		<IconCard
-		  avatarSrc={tree.Image ?? '/static/tree-placeholder.svg'}
-		  avatarFallback="🌳"
-		  title={tree.TreeName}
-		  description={`Planted by: ${tree.PlantedBy ?? 'Unknown'}`}
-		>
-		<svelte:fragment slot="content">
-			<form method="POST" on:submit|preventDefault={confirmDelete} action="?/delete">
-			  <input type="hidden" name="treeId" value={tree.Id} />
-			  <Button variant="destructive" name="delete" type="submit">Delete</Button>
-			</form>
-		  </svelte:fragment>
-		  
-		</IconCard>
-	  {/each}
-{:else}
-  <p>No trees found.</p>
-{/if}
+			{#each trees as tree}
+				<IconCard
+					avatarSrc={tree.Image ?? '/static/tree-placeholder.svg'}
+					avatarFallback="🌳"
+					title={tree.TreeName}
+					description={`Planted by: ${tree.PlantedBy ?? 'Unknown'}`}
+				>
+					<svelte:fragment slot="content">
+						<form method="POST" on:submit|preventDefault={confirmDelete} action="?/delete">
+							<input type="hidden" name="treeId" value={tree.Id} />
+							<Button variant="destructive" name="delete" type="submit">Delete</Button>
+						</form>
+					</svelte:fragment>
+				</IconCard>
+			{/each}
+		{:else}
+			<p>No trees found.</p>
+		{/if}
 	</main>
 </page>
 
