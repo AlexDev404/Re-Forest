f9a69837e8051abb7dd7d5540d58e80d83ba7260
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-05-02 04:17:02 +0000

    return neon's config back to original state

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,13 +1,11 @@
-import { drizzle } from 'drizzle-orm/neon-http';
+import { env } from '$env/dynamic/private';
 import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-import dotenv from 'dotenv';
 
-dotenv.config();  // Load environment variables from .env
-
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = neon(process.env.DATABASE_URL);
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = neon(env.DATABASE_URL);
 
 export const db = drizzle(client, {
-  schema
+	schema
 });
