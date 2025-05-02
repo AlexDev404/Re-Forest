9a624c58b3d6fe539ea340e1632480df851e1490
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-05-02 13:59:01 +0000

    Final checks

---
+++ src/lib/components/vendor/ui/button/button-span.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { cn } from '$lib/components/vendor/shadcn/utils';
+	export let href: string | undefined = undefined;
+	let className: string | undefined = undefined;
+	export { className as class };
+</script>
+
+<a
+	{href}
+	{...$$restProps}
+	class={cn(
+		'inline-flex h-9 w-full items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+		className
+	)}
+>
+	<slot />
+</a>
