878dbf5465eaae7c0b3fd45ee727f0ba7d2e6246
amilcar-vasquez <2022156707@ub.edu.bz>
2025-05-10 00:33:57 -0600

    push notifications fully implemented and working

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,20 +1,30 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging.js');
 
-firebase.initializeApp({
+// Initialize Firebase in the service worker
+const firebaseConfig = {
   apiKey: "AIzaSyDOqj153oV9vP5cfXx9stQSKXnQxjy1JuQ",
   authDomain: "refores-71a01.firebaseapp.com",
   projectId: "refores-71a01",
-  storageBucket: "refores-71a01.firebasestorage.app",
+  storageBucket: "refores-71a01.appspot.com",
   messagingSenderId: "796033762691",
   appId: "1:796033762691:web:3a53543dc6c58eeb6c22b6"
-});
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
+// This will handle background notifications
 messaging.onBackgroundMessage(function (payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const { title, ...rest } = payload.notification;
-  self.registration.showNotification(title, rest);
+  console.log("Received background message ", payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
