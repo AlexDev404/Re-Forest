ea46fb2983af9b40b368eb69c6de620161899968
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-05-11 04:29:24 +0000

    Add Firebase credentials into `.env` file

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging-compat.js');
 
 // Initialize Firebase in the service worker
 const firebaseConfig = {
