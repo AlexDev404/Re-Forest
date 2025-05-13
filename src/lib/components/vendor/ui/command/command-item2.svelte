7ea5cc08c64402cbe3f5e15e9e860b8114c97466
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-05-13 09:16:11 +0000

    feat: Upgrade components to Svelte 5 / Update layout

--- src/lib/components/vendor/ui/command/command-item2.svelte
+++ src/lib/components/vendor/ui/command/command-item2.svelte
@@ -14,7 +14,7 @@
 		'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none hover:bg-accent focus:bg-accent aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
 		className
 	)}
-	{...$$restProps}
+	{...restProps}
 >
 	<slot />
 </div>
---
+++ src/routes/reports/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<section class="h-full w-full pb-16">
+	{@render children?.()}
+</section>
